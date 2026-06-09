acf3493750ff434320ad74d39ce14b73e130854b
Guillermo <g.parra.lopez@protonmail.com>
2026-06-09 15:17:03 +0200

    Implemento SpringDataCardRepository y CardRepositoryImpl

--- infrastructure/src/main/java/es/um/pds/tableros/infrastructure/persistence/CardRepositoryImpl.java
+++ infrastructure/src/main/java/es/um/pds/tableros/infrastructure/persistence/CardRepositoryImpl.java
@@ -1,14 +1,40 @@
 package es.um.pds.tableros.infrastructure.persistence;
 
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
+import es.um.pds.tableros.domain.board.BoardId;
+import es.um.pds.tableros.domain.card.Card;
+import es.um.pds.tableros.domain.card.CardId;
 import es.um.pds.tableros.domain.ports.output.CardRepository;
+import es.um.pds.tableros.infrastructure.mappers.CardMapper;
 
 @Repository
 public class CardRepositoryImpl implements CardRepository {
 
-    // TODO: Inyectar la interfaz de Spring Data JPA (ej. SpringDataCardRepository).
-    
-    // TODO: Implementar los métodos de CardRepository (save, findById).
-    // TODO: Traducir el objeto 'Card' del dominio a 'CardEntity' antes de guardar en BD.
+    @Autowired
+    private SpringDataCardRepository springDataCardRepository;
+
+    @Autowired
+    private CardMapper cardMapper;
+
+    @Override
+    public void save(Card card) {
+        springDataCardRepository.save(cardMapper.toEntity(card));
+    }
+
+    @Override
+    public Optional<Card> findById(CardId id) {
+        return springDataCardRepository.findById(id.value())
+                .map(cardMapper::toModel);
+    }
+
+    @Override
+    public List<Card> findByBoardId(BoardId boardId) {
+        return springDataCardRepository.findByBoardId(boardId.value())
+                .stream()
+                .map(cardMapper::toModel)
+                .toList();
+    }
 }
---
+++ infrastructure/src/main/java/es/um/pds/tableros/infrastructure/persistence/SpringDataCardRepository.java
@@ -0,0 +1,9 @@
+package es.um.pds.tableros.infrastructure.persistence;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import es.um.pds.tableros.infrastructure.persistence.jpa.entity.CardEntity;
+
+public interface SpringDataCardRepository extends JpaRepository<CardEntity, String> {
+    List<CardEntity> findByBoardId(String boardId);
+}
