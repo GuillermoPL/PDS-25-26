309771a427a8261c0c99956d94714ed2c00a295f
Juan Paredes <juan.paredesp@um.es>
2026-06-16 21:51:28 +0200

    Doc. Servicio de Tarjetas

--- application/src/main/java/es/um/pds/tableros/application/usecases/events/CardMovidaEvent.java
+++ application/src/main/java/es/um/pds/tableros/application/usecases/events/CardMovidaEvent.java
@@ -1,7 +1,15 @@
 package es.um.pds.tableros.application.usecases.events;
 
+/**
+ * @brief Evento de Aplicación (Application Event) que notifica el traslado exitoso de una tarjeta.
+ * Se publica a través del bus de eventos de Spring para que otros servicios 
+ * (como la automatización) reaccionen sin acoplarse directamente al CardService.
+ * @param boardId Identificador del tablero donde ocurrió el movimiento.
+ * @param cardId Identificador de la tarjeta movida.
+ * @param listaDestinoId Identificador de la lista final donde aterrizó la tarjeta.
+ */
 public record CardMovidaEvent(
-	    String boardId,
-	    String cardId,
-	    String listaDestinoId
-	) {}
+        String boardId,
+        String cardId,
+        String listaDestinoId
+    ) {}
